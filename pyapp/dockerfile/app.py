3dea02217b27d584e75afd6a3de3df3779761d05
Jesper Wermuth <wermuth@lundogbendsen.dk>
2016-11-18 15:31:56 +0100

    Has mysql. Has vault connector

--- pyapp/dockerfile/app.py
+++ pyapp/dockerfile/app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+from flask import request
+
+import MySQLdb as mdb
+import sys
+
+
 import hvac
 
 
@@ -7,15 +13,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    print "asdlf"
-    return "Welcome!"
+    return "Welcome!!"
 
-@app.route("/read")
+
+@app.route('/read')
 def read():
+    _vault_token = request.args.get('vault_token', 'no-token provide with vault_token=TOKEN')
     client = hvac.Client()
     client = hvac.Client(url='http://localhost:8200')
-    client = hvac.Client(url='http://localhost:8200', token=os.environ['VAULT_TOKEN'])
-    return "Read!"
+    client = hvac.Client(url='http://localhost:8200', token=_vault_token)
+    return "Read! " + _vault_token
+
+
+@app.route('/connect')
+def connect():
+    try:
+        con = mdb.connect('localhost', 'testuser', 'test623', 'testdb')
+
+        cur = con.cursor()
+        cur.execute("SELECT VERSION()")
+
+        ver = cur.fetchone()
+
+        print "Database version : %s " % ver
+
+    except mdb.Error, e:
+
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        sys.exit(1)
+
+    finally:
+        if con:
+            con.close()
 
 
 if __name__ == "__main__":
