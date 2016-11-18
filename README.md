6e7cf50de43bac1697f779b0d26920f3cf3547c6
Jens Peter Secher <jpsecher@gmail.com>
2016-11-18 15:19:47 +0100

    Unseal vault.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # app-db-vault
 An application that connects to the database using credentiels provided from Vault. Thats it.
+
+## First time setup
+
+    $ docker-compose up -d
+    $ ./vault/unseal-vault.sh
+
---
+++ vault/unseal-vault.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Setup environment
+shopt -s expand_aliases
+# TODO: extract main-secrets from docker/awk.
+alias vault='docker exec -it main-secrets vault "$@"'
+export VAULT_HOST=$(echo $DOCKER_HOST | grep -Eo '\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'):8200
+
+# Initialize
+OUTPUT="$( { vault init -key-shares=1 -key-threshold=1 | head -n2; } 2> /dev/null)"
+
+VAULT_KEY=$(echo "$OUTPUT" | grep 'Key' | awk -F': ' '{print $2}')
+
+echo $VAULT_KEY
+
+export VAULT_TOKEN=$(echo "$OUTPUT" | grep 'Root Token' | awk -F': ' '{print $2}' | tr -d )
+
+# Unseal
+vault unseal $VAULT_KEY
