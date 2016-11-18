da99e55d5e4a7abf30e2826220aa61a254a2b55b
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-11-18 14:44:41 +0100

    Adding a test database schema

---
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,10 @@
+create database vaulthackathon;
+use vaulthackathon;
+
+CREATE TABLE persons
+(
+id INTEGER AUTO_INCREMENT,
+name TEXT,
+age DATETIME,
+PRIMARY KEY (id)
+) COMMENT='this is a super test table';
